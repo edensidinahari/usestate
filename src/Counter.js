61d27abfdc5f09bd33b1540990f5c59baf1b457d
Eden Sidi <edenahari@gmail.com>
2022-11-22 13:47:09 +0200

    new1

--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,26 @@
+import{useState} from "react"
 function Counter() {
-  return (
+  const [count, setCount] = useState(1)
+  function incr (){
+    setCount(
+        function (oldcount){
+            return oldcount+1
+        }
+    )
+  }
+  function reset(){
+    setCount(
+        function(){
+            return 1
+        }
+    )
+  }
+    return (
     <div> 
-      <h1> Counter</h1>
+      <h1> Counter {count}</h1>
+      <button onClick = {incr}> Click here to add 1 to the counter</button>
+      <p/>
+      <button onClick = {reset}> Click here to reset the counter</button>
     </div>
   );
 }
