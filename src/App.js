93bb7ac97d24a4232b66c07e8671acffadb09c2f
Eden Sidi <edenahari@gmail.com>
2022-11-22 13:45:53 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (
     <div className="App">
       Usestatehhs
+      <Counter/>
     </div>
   );
 }
