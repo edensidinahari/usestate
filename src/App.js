b8faaa5232d7d9b811b1b02567c6a91f228d81d9
Eden Sidi <edenahari@gmail.com>
2022-12-13 14:56:03 +0200

    new 13.12

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
 import './App.css';
-import Counter from './Counter';
-import{useState} from"react";
+import {useState} from 'react';
+import Checker from './Checker';
+
 
 function App() {
-  const[delta1,setDelta]=useState(1)
-  const[max_num,setMax]=useState(1)
-  const[max_num_reached,setMaxReach]=useState(0)
 
+  const [password, setPassword] = useState("")
+  const [disable, setDisabled] = useState(true)
 
-  function handleMax(e){
-    setMax(Number(e.target.value))
+  function handlePassword(inputPass){
+    setPassword(String(inputPass.target.value))
   }
-  function handleDelta(e){
-    setDelta(Number(e.target.value))
-  }
-  function getDataFromSon(data){
-    if (max_num_reached>data) return max_num_reached
-    setMaxReach(data)
-  }
+
   return (
     <div className="App">
-      <p>Choose how much to add</p>
-      <input type="number" value={delta1} onChange={handleDelta}></input>
-      <p>Choose maximum number</p>
-      <input type="number" value={max_num} onChange={handleMax}></input>
-      <p>maximum value reached {max_num_reached}</p>
-      <Counter delta={delta1} max_num={max_num} getData={getDataFromSon}></Counter>
-      <Counter delta={delta1} max_num={max_num} getData={getDataFromSon}></Counter>
+      <p>
+        <input type="password" value={password} onChange={handlePassword}></input>
+        <button disabled={disable}>Submit</button>
+        <Checker password={password} checkPassword={handlePassword} setPassword={setPassword} setDisabled={setDisabled}/>
+      </p>
+
     </div>
   );
 }
